383eeba89dff6e86b83316f13d9484c51472910c
Franklin <franklin.mamani.m@tecsup.edu.pe>
2024-05-06 12:02:32 -0700

    Initial Commit

---
+++ MamaniCruzColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MamaniCruzColeccionDeJuegos
+//
+//  Created by Franklin Junnior on 5/6/24.
+//  Copyright © 2024 franklin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
